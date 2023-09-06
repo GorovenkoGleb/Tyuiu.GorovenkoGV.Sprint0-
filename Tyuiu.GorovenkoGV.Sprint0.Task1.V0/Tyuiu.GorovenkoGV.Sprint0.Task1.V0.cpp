220817829fc7921af287cf6db84309b518000e50
GorovenkoGleb <gorovenkogv@outlook.com>
2023-09-06 20:42:54 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.GorovenkoGV.Sprint0.Task1.V0/Tyuiu.GorovenkoGV.Sprint0.Task1.V0.cpp
@@ -0,0 +1,17 @@
+﻿// Tyuiu.GorovenkoGV.Sprint0.Task1.V0.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Покажите мне свои бумаги : ";
+    string a;
+    cin >> a;
+    cout << "Хорошо, а теперь скажите сколько вам лет :";
+    int v;
+    cin >> v;
+    cout << "Хорошо, теперь я размещу здесь чтобы все знали :) ->" << v;
+    return 0;
+}
