7623897178a75afb55b4c91cbfc12ea3af34d8a2
GorovenkoGleb <gorovenkogv@outlook.com>
2023-09-09 04:36:15 +0500

    Task3

---
+++ Tyuiu.GorovenkoGV.Sprint0.Task3.V0.Lib/Tyuiu.GorovenkoGV.Sprint0.Task3.V0.Lib.cpp
@@ -0,0 +1,27 @@
+﻿// Tyuiu.GorovenkoGV.Sprint0.Task3.V0.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: Это пример библиотечной функции.
+class Service1:public ISprint0Task3
+{
+
+	//Inherited via ISprint0Task3V0
+
+	virtual int SummV3(int a, int b, int c)
+	{
+		return a + b + c; //решение
+	};
+
+
+
+
+	// Inherited via ISprint0Task2V1
+
+	//virtual
+
+
+};
